1cd9c945af6faef4adf9f517b3c0dc6ba22f1e45
Alexander Dollard <alexdollard141@gmail.com>
2022-11-13 13:03:27 -0600

    sidebarList structure

--- studio/components/SidebarList.js
+++ studio/components/SidebarList.js
@@ -1,7 +1,20 @@
 import S from '@sanity/desk-tool/structure-builder';
+import { MdStar } from 'react-icons/md';
 
 function SidebarList () {
-    return "this is test";
+    return S.list()
+        .title('TechHub-Blog')
+        .items([
+            S.listItem()
+                .title('Featured')
+                .id('featuredItems')
+                .icon(MdStar)   
+                .child(S.editor()
+                .schemaType('featured')
+                .documentId('featuredItems')),
+                ...S.documentTypeListItems()
+                .filter((item) => item.getId() !== 'featured'),
+        ]);
 }
 
 export default SidebarList;
