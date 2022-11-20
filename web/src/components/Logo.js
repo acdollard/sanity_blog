64f5199c0ec9667364aa513df300d4ef2855536a
Alexander Dollard <alexdollard141@gmail.com>
2022-11-20 12:48:51 -0600

    added navbar, mobile nav

---
+++ web/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GrTechnology } from 'react-icons/gr';
+import LogoStyles from '../styles/LogoStyles';
+
+function Logo() {
+  return (
+    <LogoStyles>
+      <GrTechnology to={''}/>
+    </LogoStyles>
+  )
+}
+
+export default Logo
