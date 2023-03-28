8a8173b2525cb7a9076df155745602ab24c65551
Alexander Dollard <alexdollard141@gmail.com>
2023-03-27 19:02:27 -0500

    stubs out authors page

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -6,7 +6,7 @@ const singleBlogTemplate = require.resolve('./src/templates/single-blog.js');
 const blogListTemplate = require.resolve('./src/templates/blog-list.js');
 const singleCategoryTemplate = require.resolve('./src/templates/single-category.js');
 const categoryListTemplate = require.resolve('./src/templates/category-list.js');
-
+const authorListTemplate = require.resolve('./src/templates/author-list.js');
 
   const { createPage } = actions;
   const result = await graphql(`
@@ -27,12 +27,21 @@ const categoryListTemplate = require.resolve('./src/templates/category-list.js')
         }
       }
     }
+    allSanityAuthor {
+      nodes {
+        id
+        slug {
+          current
+        }
+      }
+    }
   }
   `);
 
   if (result.errors) throw result.errors;
   const blogs = result.data.allSanityBlog.nodes;
   const categories = result.data.allSanityCategory.nodes;
+  const authors = result.data.allSanityAuthor.nodes
 
   //single blog pages
       blogs.forEach((blog) => {
@@ -77,6 +86,21 @@ const categoryListTemplate = require.resolve('./src/templates/category-list.js')
           limit: postsPerPage,
           offset: index * postsPerPage,
           numberOfPages: totalCategoryListPages,
+          currentPage: index + 1,
+        }
+      })
+    })
+
+    // author-list pages
+    const totalAuthorListPages = Math.ceil(authors.length / postsPerPage);
+    Array.from({ length: totalAuthorListPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? '/authors' : `/authors/${index + 1}`,
+        component: authorListTemplate,
+        context: {
+          limit: postsPerPage,
+          offset: index * postsPerPage,
+          numberOfPages: totalAuthorListPages,
           currentPage: index + 1,
         }
       })
