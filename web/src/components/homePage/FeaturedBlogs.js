1e1a89cc8c8de299d16e4f6ee0d4bf594a528fa8
Alexander Dollard <alexdollard141@gmail.com>
2024-04-21 18:57:08 -0500

    comments out unused component featuredblog

--- web/src/components/homePage/FeaturedBlogs.js
+++ web/src/components/homePage/FeaturedBlogs.js
@@ -6,44 +6,44 @@ import ParagraphText from "../typography/ParagraphText";
 import  BlogGrid  from "../blog/BlogGrid";
 
 function FeaturedBlogs() {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      allSanityFeatured(filter: { _id: { eq: "featuredItems" } }) {
-        nodes {
-          blog {
-            title  
-            id
-            publishedAt
-            categories {
-              title
-              slug {
-                current
-              }
-            }
-            coverImage {
-              alt
-              asset {
-                gatsbyImageData
-              }
-            }
-            slug {
-              current
-            }
-          }
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query MyQuery {
+  //     allSanityFeatured(filter: { _id: { eq: "featuredItems" } }) {
+  //       nodes {
+  //         blog {
+  //           title  
+  //           id
+  //           publishedAt
+  //           categories {
+  //             title
+  //             slug {
+  //               current
+  //             }
+  //           }
+  //           coverImage {
+  //             alt
+  //             asset {
+  //               gatsbyImageData
+  //             }
+  //           }
+  //           slug {
+  //             current
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
 
-  const featuredBlogs = data.allSanityFeatured.nodes[0].blog;
+  // const featuredBlogs = data.allSanityFeatured.nodes[0].blog;
 
   return (
     <FeaturedBlogsStyles>
-      <SectionTitle>Featured Blogs</SectionTitle>
+      {/* <SectionTitle>Featured Blogs</SectionTitle>
       <ParagraphText className="featuredBlogs__text">
         
       </ParagraphText>
-      <BlogGrid blogs={featuredBlogs}/>
+      <BlogGrid blogs={featuredBlogs}/> */}
     </FeaturedBlogsStyles>
   );
 }
